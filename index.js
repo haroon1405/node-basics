275890699b1c455fc1249279c9f81c9dabec332c
haroon1405 <mohammedharoonmh2000@gmail.com>
2024-01-17 15:10:33 +0300

    final changes

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 import express from "express";
+import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const port = 3000;
+const filePath = path.join(__dirname, 'node_basics.html');
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Hello</h1>')
+    res.sendFile(filePath)
 })
 
 app.get('/home',(req,res)=>{
-    res.send('<h1>Welcome To Home</h1>')
+    res.send('<h1>Welcome To Home</h1><a href="/">Go Back</a>')
 })
 
 app.listen(port,()=>{
--- js-basics/index.js
+++ js-basics/index.js
@@ -1,133 +1,133 @@
-// console.log('Hello World');
+console.log('Hello World');
 
-// let obj = {
-//     name: "Haroon",
-//     age: 18
-// };
+let obj = {
+    name: "Haroon",
+    age: 18
+};
 
-// console.log(`Hi ${obj.name}`);
-// console.log(`5 != 5 is ${5 != 5}`);
-// console.log(`${(5>9)?'Morning':'Night'}`);
+console.log(`Hi ${obj.name}`);
+console.log(`5 != 5 is ${5 != 5}`);
+console.log(`${(5>9)?'Morning':'Night'}`);
 
-// for (let e of "Hello"){
-//     console.log(e);
-// }
+for (let e of "Hello"){
+    console.log(e);
+}
 
-// for (let key in obj){
-//     console.log(`${key} : ${obj[key]}`)
-// }
+for (let key in obj){
+    console.log(`${key} : ${obj[key]}`)
+}
 
-// let ages = [12,45,18,20];
+let ages = [12,45,18,20];
 
-// ages.forEach((age)=>{
-//     console.log(age)
-// })
-// ages.splice(1,2,47,48)
+ages.forEach((age)=>{
+    console.log(age)
+})
+ages.splice(1,2,47,48)
 
-// console.log(ages)
+console.log(ages)
 
-// let arr = [5,6,2,1,3];
+let arr = [5,6,2,1,3];
 
-// let max = arr.reduce((prev,curr)=>{
-//     return ((curr>prev)?curr:prev)
-// })
+let max = arr.reduce((prev,curr)=>{
+    return ((curr>prev)?curr:prev)
+})
 
-// console.log(max)
+console.log(max)
 
-// let prod = arr.reduce((prev,curr)=>{
-//     return prev*curr
-// })
+let prod = arr.reduce((prev,curr)=>{
+    return prev*curr
+})
 
-// console.log(prod)
-
-
-// class Human{
-//     constructor(name,age){
-//         this.name = name;
-//         this.age = age;
-//     }
-//     alive(){
-//         constructor.log('Is alive')
-//     }
-// }
+console.log(prod)
 
 
-// class Student extends Human{
-//     constructor(name,age,cgpa){
-//         super(name,age)
-//         this.cgpa = cgpa;
-//     }
-//     speak(){
-//         console.log('speak')
-//     }
-//     talk(){
-//         console.log('talk')
-//     }
-// }
-
-// let student1 = new Student('Haroon',18)
+class Human{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    alive(){
+        constructor.log('Is alive')
+    }
+}
 
 
-// Callbacks
-// function getData(dataID,callback){
-//     setTimeout(()=>{
-//         console.log(`Data - ${dataID}`)
-//         if(callback){
-//             callback();
-//         }
-//     },2000)
-// }
+class Student extends Human{
+    constructor(name,age,cgpa){
+        super(name,age)
+        this.cgpa = cgpa;
+    }
+    speak(){
+        console.log('speak')
+    }
+    talk(){
+        console.log('talk')
+    }
+}
 
-// getData(123,()=>{
-//     getData(456,()=>{
-//         getData(789)
-//     })
-// })
-
-// Promises
-// function getData(dataID){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(() => {
-//             resolve(dataID)
-//         }, 2000);
-//     })
-// }
-
-// getData(123)
-//     .then((result) => {
-//     console.log(`Data - ${result}`)
-//     return getData(456) })
-//     .then((result) => {
-//     console.log(`Data - ${result}`) })
-//     .catch((err)=>{
-//         console.log(err)
-//     })
-
-// Async Await
-// function getData(dataID){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(() => {
-//             resolve(dataID)
-//         }, 2000);
-//     })
-// }
-
-// async function data(){
-//     let res1 = await getData(123);
-//     console.log(`Data - ${res1}`)
-//     let res2 = await getData(456);
-//     console.log(`Data - ${res2}`)
-// }
-// data()
+let student1 = new Student('Haroon',18)
 
 
-// APIS
-// async function getapi(){
-//     let result = await fetch('https://cat-fact.herokuapp.com/facts')
-//     let response = await result.json()
-//     console.log(response)
-// }
-// getapi()
+Callbacks
+function getData(dataID,callback){
+    setTimeout(()=>{
+        console.log(`Data - ${dataID}`)
+        if(callback){
+            callback();
+        }
+    },2000)
+}
+
+getData(123,()=>{
+    getData(456,()=>{
+        getData(789)
+    })
+})
+
+Promises
+function getData(dataID){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve(dataID)
+        }, 2000);
+    })
+}
+
+getData(123)
+    .then((result) => {
+    console.log(`Data - ${result}`)
+    return getData(456) })
+    .then((result) => {
+    console.log(`Data - ${result}`) })
+    .catch((err)=>{
+        console.log(err)
+    })
+
+Async Await
+function getData(dataID){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve(dataID)
+        }, 2000);
+    })
+}
+
+async function data(){
+    let res1 = await getData(123);
+    console.log(`Data - ${res1}`)
+    let res2 = await getData(456);
+    console.log(`Data - ${res2}`)
+}
+data()
+
+
+APIS
+async function getapi(){
+    let result = await fetch('https://cat-fact.herokuapp.com/facts')
+    let response = await result.json()
+    console.log(response)
+}
+getapi()
 
 fetch('https://cat-fact.herokuapp.com/facts')
 .then((response)=>{
@@ -138,19 +138,19 @@ fetch('https://cat-fact.herokuapp.com/facts')
 ])
 
 
-// let divElmnt = document.querySelector('.inner-div');
-// let btn = document.querySelector('.button-js');
+let divElmnt = document.querySelector('.inner-div');
+let btn = document.querySelector('.button-js');
 
-// btn.addEventListener("click", () => {
-//     if (divElmnt.innerText == 'hello') {
-//         divElmnt.innerText = "Hello World";
-//         document.querySelector('.new-btn').remove()
-//     }
-//     else {
-//         divElmnt.innerText = "hello";
-//         let newElmnt = document.createElement('div');
-//         newElmnt.classList.add('new-btn', 'bg-green-500','w-fit','mx-auto', 'mt-2', 'text-white', 'p-2')
-//         newElmnt.innerText = 'Click on the button'
-//         btn.after(newElmnt)
-//     }
-// });
+btn.addEventListener("click", () => {
+    if (divElmnt.innerText == 'hello') {
+        divElmnt.innerText = "Hello World";
+        document.querySelector('.new-btn').remove()
+    }
+    else {
+        divElmnt.innerText = "hello";
+        let newElmnt = document.createElement('div');
+        newElmnt.classList.add('new-btn', 'bg-green-500','w-fit','mx-auto', 'mt-2', 'text-white', 'p-2')
+        newElmnt.innerText = 'Click on the button'
+        btn.after(newElmnt)
+    }
+});
